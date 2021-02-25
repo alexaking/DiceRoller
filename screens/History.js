626e9878d2b0d3421300543ed5f0a8f1b23e7e0d
Alexa King <kingjalexa@gmail.com>
2021-02-25 16:44:35 -0500

    some changes

--- screens/History.js
+++ screens/History.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
+import history from './DiceRoller'
 
 class History extends React.Component {
     static navigationOptions = ({ navigation }) => {
@@ -11,6 +12,7 @@ class History extends React.Component {
       return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
           <Text>History</Text>
+          console.log("here's your history!")
         </View>
       );
     }
